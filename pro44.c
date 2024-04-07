9f69471bc37ea0d9e849c53d3497080309577b25
NIKITA KUMARI <159749602+Nikita-Kumari94@users.noreply.github.com>
2024-04-07 17:54:07 +0530

    Add files via upload

---
+++ pro44.c
@@ -0,0 +1,62 @@
+/* 23/CS/283
+NIKITA KUMARI */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_SIZE 100
+
+struct stack{
+    char arr[MAX_SIZE];
+    int top;
+};
+
+void initialize(struct stack *s){
+    s->top = -1;
+}
+
+bool isEmpty(struct stack *s){
+    return s->top == -1;
+}
+
+void push(struct stack *s, char c){
+    s->arr[++s->top] = c;
+}
+
+char pop(struct stack *s){
+    return s->arr[s->top--];
+}
+
+bool check_parentheses(const char *exp)
+{
+    struct stack s;
+    initialize(&s);
+    while (*exp)
+    {
+        char c = *exp++;
+        if (c == '(' || c == '[' || c == '{')
+        {
+            push(&s, c);
+        }
+        else if (c == ')' || c == ']' || c == '}')
+        {
+            if (isEmpty(&s))
+                return false;
+            char topc = pop(&s);
+            if ((c == ')' && topc != '(') || (c == ']' && topc != '[') || (c == '}' && topc != '{'))
+                return false;
+        }
+    }
+    return isEmpty(&s);
+}
+
+int main()
+{
+    const char *exp = "{[(])]}";
+    if (check_parentheses(exp))
+        printf("Valid parentheses\n");
+    else
+        printf("Invalid parentheses\n");
+    return 0;
+}
---
+++ pro45.c
@@ -0,0 +1,25 @@
+/* 23/CS/283
+NIKITA KUMARI */
+
+#include <stdio.h>
+
+void next_gr(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        int ng = -1;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+                ng = arr[j];
+                break;
+            }
+        }
+        printf("Next greater element to the right of %d is %d\n", arr[i], ng);
+    }
+}
+
+int main() {
+    int arr[] = {4, 2, 11, 5, 8, 10, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    next_gr(arr, n);
+    return 0;
+}
